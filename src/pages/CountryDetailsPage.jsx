f953bd2c0dbe71b28ec0dd1a56b42c29bbc96ef4
“Eva <evacmak@outlook.pt>
2024-05-25 16:47:02 +0100

    done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,77 @@
-function CountryDetails() {}
+    import { useState, useEffect } from "react";
+    import axios from "axios";
+    import { Link, useParams } from "react-router-dom";
 
-export default CountryDetails;
+
+    function CountryDetails() {
+
+        const [country, setCountry] = useState(null);
+        const {countryId} = useParams();
+
+
+        const getCountryDetails = async (id) => {
+    
+            try {
+              const response = await axios.get (`https://ih-countries-api.herokuapp.com/countries/${id}`);
+              console.log(response.data);
+              setCountry(response.data);
+              
+            } catch (error) {
+            console.log('error fetching countries');
+            }
+          };
+
+        useEffect(() => {
+            getCountryDetails(countryId);
+        }, [countryId]);
+
+
+        return (
+            <div className="container">
+            <p style={{fontSize: '24px', fontWeight: 'bold'}}>Country Details</p>
+
+            {!country && <p>Loading...</p>}
+            {country && (
+            
+            <>
+                <h1>{country && country.name.common}</h1>
+
+<table className="table">
+<thead></thead>
+<tbody>
+    <tr>
+    <td style={{width: '30%'}}>Capital</td>
+    <td>{country.capital}</td>
+    </tr>
+    <tr>
+    <td>Area</td>
+    <td>
+        {country.area}
+        <sup>2</sup>
+    </td>
+    </tr>
+    <tr>
+    <td>Borders</td>
+    <td>
+        <ul>
+        {country.borders.map(border => {
+            return (
+                <li key={border}>
+                    <Link to={`${border}`}>{border}</Link>
+                </li>
+            )
+        })}
+        <li><a href="/AND">Andorra</a></li>
+        </ul>
+    </td>
+    </tr>
+</tbody>
+</table>
+</>
+           ) }
+           </div>
+            
+        )
+    }
+
+    export default CountryDetails;
